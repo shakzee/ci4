8fc792305be273a04be1704ef9487f1f84cde2a6
shakzeeofficial <shakzee171@gmail.com>
2023-11-24 15:57:45 +0500

    first commit

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        echo 'pages class/controller';
+        //return view('welcome_message');
+    }
+}
---
+++ app/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class PagesController extends BaseController
+{
+    public function index()
+    {
+        echo 'pagescontroller';
+    }
+}
